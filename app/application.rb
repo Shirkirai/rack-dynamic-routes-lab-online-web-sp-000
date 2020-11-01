ee5352919c8faf169ae005c7195f890cfc5e5b13
Jung Lee <lee.jung.s95@gmail.com>
2020-11-01 22:02:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,7 @@ class Application
         resp.write "Item not found"
         resp.status = 400
       else
-        resp.write @@items.each{|item| item_price = item.price}
+        resp.write @@items.each{|item| item_price = item.price.to_s}
         resp.status = 200
       end
     else

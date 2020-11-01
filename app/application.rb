1359044608e9918d062701d48a0a32ef59939bba
Jung Lee <lee.jung.s95@gmail.com>
2020-11-01 21:50:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,8 +9,8 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      
-      
+
+
       item_price = req.path.split("/items/").last.price.to_s
       binding.pry
       @@items.each do |item|

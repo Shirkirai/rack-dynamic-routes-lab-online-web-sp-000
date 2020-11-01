e0eaa6300d0ab61b5598e989611b39096c7ec6f9
Jung Lee <lee.jung.s95@gmail.com>
2020-11-01 22:21:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = @@items.find{|item| item.name == item_name}
+      if item = @@items.find{|item| item.name == item_name}
       resp.write item_name.price
     else
       resp.write "Item not found"

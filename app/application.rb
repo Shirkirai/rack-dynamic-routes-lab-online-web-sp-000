6171119f6382f1279dba535a104ac4c5d8425730
Jung Lee <lee.jung.s95@gmail.com>
2020-11-01 22:19:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,9 +12,10 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
     else
-        resp.write "Item not found"
-        resp.status = 400
-      end
+      resp.write "Item not found"
+      resp.status = 400
+    end
+    
     else
       resp.write "Route not found"
       resp.status = 404

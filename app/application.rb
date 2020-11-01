31723340fb1f81b944a1b0c6965dc9448253aa2f
Jung Lee <lee.jung.s95@gmail.com>
2020-11-01 21:35:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,16 +8,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item_price = req.path.split("/items/").last
-      item = @@items.find{|i| i.price == item_price}
-
-      resp.write item.price
-    else
-      resp.write "Item not found"
-      resp.status = 400
     end
-
-    resp.finish
 
 
 

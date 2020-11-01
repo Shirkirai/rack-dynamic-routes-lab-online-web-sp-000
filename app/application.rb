a681db0ec6f3e25393c03beba0e5eebfbc77c199
Jung Lee <lee.jung.s95@gmail.com>
2020-11-01 21:44:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
     if req.path.match(/items/)
       @@items.each do |item|
         binding.pry
-        resp.write item.price
+        resp.write item.price.to_s
       end
     end
 

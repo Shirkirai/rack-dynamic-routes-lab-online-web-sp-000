43a3b46818af5a6ed91ab0346518ea6e34ce6fde
Jung Lee <lee.jung.s95@gmail.com>
2020-11-01 21:50:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,10 +9,11 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
+      binding.pry
       
       item_price = req.path.split("/items/").last.price.to_s
       resp.write item_price
-      
+
     end
 
 
